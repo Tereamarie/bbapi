b04a1db9d80436b216764573ccb0d099b23888ff
thebrianemory <the@brianemory.com>
2017-09-12 07:55:39 -0700

    Set local db settings

--- config/dev.exs
+++ config/dev.exs
@@ -51,8 +51,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :bb_api, BbApi.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "brianemory",
+  password: "",
   database: "bb_api_dev",
   hostname: "localhost",
   pool_size: 10
--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,8 @@ config :logger, level: :warn
 # Configure your database
 config :bb_api, BbApi.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "brianemory",
+  password: "",
   database: "bb_api_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
